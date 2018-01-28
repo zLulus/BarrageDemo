6051877033babcda41a69d1f27e85e5e55314b7f
曾璐 <865514198@qq.com>
2018-01-28 19:39:45 +0800

    瀑布流50%

--- BarrageDemo/MainWindow.xaml.cs
+++ BarrageDemo/MainWindow.xaml.cs
@@ -23,12 +23,31 @@ namespace BarrageDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 每行的起始高度
+        /// </summary>
         private List<double> heightList;
+        /// <summary>
+        /// 每行的弹幕未显示长度
+        /// </summary>
+        private List<double> lengthList;
+        /// <summary>
+        /// 当前可以加弹幕的位置（行数）集合
+        /// </summary>
+        private List<int> locations;
         private List<Color> colorList;
         private DispatcherTimer timer;
         private Random random;
-        //存储当前没有展现的弹幕
+        /// <summary>
+        /// 存储当前没有展现的弹幕
+        /// </summary>
         private List<MessageInformation> messages;
+        /// <summary>
+        /// 弹幕行数
+        /// </summary>
+        private int rowCount = 3;
+        private bool isOver;
+        private object locker;
 
         public MainWindow()
         {
@@ -72,25 +91,60 @@ namespace BarrageDemo
 
         public void ShowMessage(object sender, EventArgs e)
         {
-            //todo 卡顿的问题->间距缩小
-            Task.Run(() => {
-                System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            //如果时间间隔太短，可能出现字幕叠加，是因为同一时间几个计时器都在用数据 ->lock
+            if (isOver)
+            {
+                //异步解决卡顿的问题
+                Task.Run(() =>
                 {
-                    //查询新的弹幕
-                    GetMessages();
-                    //显示弹幕
-                    Barrage(GetTopThreeMessages());
+                    lock (locker)
+                    {
+                        isOver = false;
+                    }
+                    //非UI线程调用UI组件
+                    System.Windows.Application.Current.Dispatcher.Invoke(async () =>
+                    {
+                        //查询新的弹幕
+                        await GetMessages();
+                        ReduceLengthList();
+                        //显示弹幕
+                        Barrage(GetTopThreeMessages());
+                        lock (locker)
+                        {
+                            isOver = true;
+                        }
+                    });
                 });
-            });
-           
+            }
+
+
+        }
+
+        /// <summary>
+        /// 按时间减少每行的未显示长度
+        /// </summary>
+        private void ReduceLengthList()
+        {
+            for (int i = 0; i < lengthList.Count; i++)
+            {
+                if (lengthList[i] > 0)
+                {
+                    //这里根据动画的速度调节，每个计时器的间隔走了X个字
+                    lengthList[i] -= 0.5;
+                }
+                //减得过多了，重置为0，认为该行已展示完成
+                if (lengthList[i] < 0)
+                {
+                    lengthList[i] = 0;
+                }
+            }
         }
 
         /// <summary>
         /// 查询新的弹幕
         /// </summary>
-        private void GetMessages()
+        private async Task GetMessages()
         {
-            //最新的弹幕
             GetMessageInformationResult newMessages = new GetMessageInformationResult();
             newMessages.result = new List<MessageInformation>();
             newMessages.result.Add(new MessageInformation() { content = "你们的" });
@@ -112,13 +166,31 @@ namespace BarrageDemo
         private List<MessageInformation> GetTopThreeMessages()
         {
             List<MessageInformation> infos = new List<MessageInformation>();
-            int number = messages.Count >= 3 ? 3 : messages.Count;
+            //查询哪些行数的字已经显示完成
+            GetAllShowLocations();
+            int number = locations.Count;
             for (int i = 0; i < number; i++)
             {
                 infos.Add(messages[i]);
             }
             messages.RemoveRange(0, number);
             return infos;
+        }
+
+        /// <summary>
+        /// 查询哪些行数的字已经显示完成
+        /// </summary>
+        private void GetAllShowLocations()
+        {
+            //重新初始化
+            locations = new List<int>();
+            for (int i = 0; i < lengthList.Count; i++)
+            {
+                if (lengthList[i] == 0)
+                {
+                    locations.Add(i);
+                }
+            }
         }
 
         /// <summary>
@@ -129,19 +201,20 @@ namespace BarrageDemo
         {
             Random random = new Random();
             //当前读取弹幕的位置
-            int location = 0;
             for (int i = 0; i < contentlist.Count(); i++)
             {
                 //获取高度位置(置顶、三分之一、三分之二)
-                double inittop = GetHeight(location);
+                double inittop = GetHeight(locations[i]);
                 //获取速度随机数
                 //double randomspeed = random.NextDouble();
                 double initspeed = 30;
                 //实例化TextBlock和设置基本属性,并添加到Canvas中
                 TextBlock textblock = new TextBlock();
-                textblock.Text = contentlist[location].content;
+                textblock.Text = contentlist[i].content;
                 textblock.FontSize = 20;
                 textblock.Foreground = GetRandomColor();
+                //记录该行的未显示字幕数
+                lengthList[locations[i]] = lengthList[locations[i]] + contentlist[i].content.Length;
                 //这里设置了弹幕的高度
                 Canvas.SetTop(textblock, inittop);
                 canvas.Children.Add(textblock);
@@ -160,7 +233,6 @@ namespace BarrageDemo
                 };
                 //启动动画
                 textblock.BeginAnimation(Canvas.LeftProperty, animation);
-                location++;
             }
 
         }
@@ -192,13 +264,25 @@ namespace BarrageDemo
             double height = canvas.ActualHeight;
             heightList.Add(0);
             //三分之一高
-            double threeHeight = height / 3.00;
-            heightList.Add(threeHeight);
-            heightList.Add(threeHeight * 2);
+            double threeHeight = height / rowCount;
+            for (int i = 1; i < rowCount; i++)
+            {
+                heightList.Add(threeHeight * i);
+            }
+
+            lengthList = new List<double>(rowCount);
+            for (int i = 0; i < rowCount; i++)
+            {
+                lengthList.Add(0);
+            }
+            locations = new List<int>(rowCount);
+
+            isOver = true;
+            locker = new object();
 
             //设置定时器
             timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 1);   //时间间隔为一秒
+            timer.Interval = new TimeSpan(0, 0, 1);   //时间间隔
             timer.Tick += new EventHandler(ShowMessage);
             timer.Start();
         }
